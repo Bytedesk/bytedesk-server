fc01d4fdc7e162128eddf74d69050ea08022a308
jackning <270580156@qq.com>
2019-02-01 19:56:41 +0800

    update

---
+++ im-in-action-tutorial/udp-chat/src/main/java/com/xiaper/UdpChatApp.java
@@ -0,0 +1,14 @@
+package com.xiaper;
+
+/**
+ * Hello world!
+ *
+ * @author xiaper.com
+ */
+public class UdpChatApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
