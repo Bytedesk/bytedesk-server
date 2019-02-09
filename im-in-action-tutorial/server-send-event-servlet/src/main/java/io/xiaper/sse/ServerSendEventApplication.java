6a0648f79776b7187c56aa598f377479b7baf649
jackning <270580156@qq.com>
2019-02-09 12:38:13 +0800

    update

--- im-in-action-tutorial/server-send-event-servlet/src/main/java/io/xiaper/sse/ServerSendEventApplication.java
+++ im-in-action-tutorial/server-send-event-servlet/src/main/java/io/xiaper/sse/ServerSendEventApplication.java
@@ -3,6 +3,10 @@ package io.xiaper.sse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author xiaper.io
+ */
 @SpringBootApplication
 public class ServerSendEventApplication {
 
---
+++ im-in-action-tutorial/server-send-event-webflux/src/main/java/io/xiaper/sse/ServerSendEventApplication.java
@@ -0,0 +1,18 @@
+package io.xiaper.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author xiaper.io
+ */
+@SpringBootApplication
+public class ServerSendEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSendEventApplication.class, args);
+	}
+
+}
+
