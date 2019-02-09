8169d348e51ccf818bdcbee23191840bc4e3c30a
jackning <270580156@qq.com>
2019-02-09 15:30:43 +0800

    update

--- im-in-action-tutorial/polling/src/main/java/io/xiaper/polling/PollingApplication.java
+++ im-in-action-tutorial/polling/src/main/java/io/xiaper/polling/PollingApplication.java
@@ -3,12 +3,15 @@ package io.xiaper.polling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author xiaper.io
+ */
 @SpringBootApplication
 public class PollingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollingApplication.class, args);
 	}
-
 }
 
---
+++ im-in-action-tutorial/server-send-event-servlet/src/main/java/io/xiaper/sse/controller/SseController.java
@@ -0,0 +1,33 @@
+package io.xiaper.sse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+/**
+ * @author bytedesk.com on 2019/2/9
+ */
+@RestController
+public class SseController {
+
+    @RequestMapping(value="/push",produces="text/event-stream;charset=utf-8")
+    @ResponseBody
+    public String push() {
+        Random r = new Random();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "data:Testing 1,2,3" + r.nextInt() +"\n\n";
+    }
+
+    @RequestMapping(value = "/sseTest", method = RequestMethod.GET)
+    public String getSSEView () {
+        return "sseTest";
+    }
+
+}
