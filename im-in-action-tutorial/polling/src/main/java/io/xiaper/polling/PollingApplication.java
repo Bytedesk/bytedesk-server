75782c433f0da65c4904cf0fac267d728bbf88b3
jackning <270580156@qq.com>
2019-02-09 11:07:28 +0800

    update

---
+++ im-in-action-tutorial/polling/src/main/java/io/xiaper/polling/PollingApplication.java
@@ -0,0 +1,14 @@
+package io.xiaper.polling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingApplication.class, args);
+	}
+
+}
+
---
+++ im-in-action-tutorial/polling/src/test/java/io/xiaper/polling/PollingApplicationTests.java
@@ -0,0 +1,17 @@
+package io.xiaper.polling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PollingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
