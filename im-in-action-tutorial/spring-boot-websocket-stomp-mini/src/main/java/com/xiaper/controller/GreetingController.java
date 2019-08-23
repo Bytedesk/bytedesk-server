c3b5bea147bd38c8fb6d2692f61956ac2dbf4dc1
jackning <270580156@qq.com>
2019-08-23 22:01:58 +0800

    update

---
+++ im-in-action-tutorial/spring-boot-websocket-stomp-mini/src/main/java/com/xiaper/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package com.xiaper.controller;
+
+import com.xiaper.model.Greeting;
+import com.xiaper.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+/**
+ *
+ * @author xiaper.com
+ */
+@Controller
+public class GreetingController {
+
+
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
---
+++ im-in-action-tutorial/spring-boot-websocket-stomp-sockjs/src/main/java/com/xiaper/springbootwebsocketsockjs/SpringBootWebsocketSockjsApplication.java
@@ -0,0 +1,14 @@
+package com.xiaper.springbootwebsocketsockjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketSockjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketSockjsApplication.class, args);
+	}
+
+}
+
